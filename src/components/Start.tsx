1cae2af5361bd95c3f5c40e2e239580362ff7bb4
dya-code <dyacode@proton.me>
2023-01-01 22:09:36 +0900

    start page

---
+++ src/components/Start.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+import Sub_bg from "../assets/sub_bg.png"
+
+function Start() {
+
+  return (
+    <div className="start bg-cover font-molu-bold overflow-x-hidden">
+
+    </div>
+  )
+}
+
+export default Start
