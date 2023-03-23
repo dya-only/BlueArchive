0fd931ce91487efe637550653da7987d68e799fe
dya-only <dyacode@proton.me>
2023-03-23 19:52:11 +0900

    pickup new design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,37 +9,37 @@ import nav from '../assets/navbar.png'
 function NavBar(props: any) {
 
   return (
-    <div className="navbar flex items-center justify-between tracking-tight fixed bg-[#f2fafd] shadow-lg rounded-b-4xl h-[60px] w-screen mb-12 z-30">
+    <div id={'navbar'} className="navbar flex items-center justify-between tracking-tight fixed bg-[#f2fafd] shadow-lg rounded-b-4xl h-[60px] w-screen mb-12 z-30">
       
       <div className="front-menu flex items-center">
-        <img className="h-[60px]" src={ nav } alt="" />
+        <img id={'navbar-img'} className="h-[60px]" src={ nav } alt="" />
 
         <div className="first fixed flex items-center justify-center">
           <Link className='z-10' to="/">
-            <button className="prev transition duration-100 active:scale-90 bg-[#456399] rounded-full w-[70px] h-[70px] m-10 mt-[85px] transition duration-300 hover:opacity-70">
-              <FontAwesomeIcon className='text-5xl text-white' icon={faArrowLeft} />
+            <button id={'prev'} className="prev transition duration-100 active:scale-90 bg-[#456399] flex justify-center items-center rounded-full w-[70px] h-[70px] m-10 mt-[85px] transition duration-300 hover:opacity-70">
+              <FontAwesomeIcon id={'prev-icon'} className='text-5xl text-white' icon={faArrowLeft} />
             </button>
           </Link>
 
-          <div className="title text-[#2c4663] tracking-tight text-4xl border-b-4 h-[47px] -ml-12 mt-3 pl-8 border-[#ffe03d]">{ props.name }</div>
-          <button className="help transition duration-100 active:scale-90 text-white bg-[#2a4566] font-bold text-xl w-8 h-8 ml-2 mt-1 rounded-md transition duration-300 hover:opacity-70">?</button>
+          <div id={'pickup-title'} className="title text-[#2c4663] tracking-tight text-4xl border-b-4 h-[47px] -ml-12 mt-3 pl-8 border-[#ffe03d]">{ props.name }</div>
+          <button id={'pickup-help'} className="help transition duration-100 active:scale-90 text-white bg-[#2a4566] font-bold text-xl w-8 h-8 ml-2 mt-1 rounded-md transition duration-300 hover:opacity-70">?</button>
         </div>
 
       </div>
  
       <div className="back-menu flex items-center justify-end">
 
-      <div className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-6"></div>
+      <div id={'pickup-sub'} className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-6"></div>
 
-        <button className="setting-btn mt-1 mr-6">
-          <FontAwesomeIcon className='text-4xl text-[#436299] transition duration-100 active:scale-90' icon={faGear} />
+        <button id={'pickup-btn-btn'} className="setting-btn mt-1 mr-6">
+          <FontAwesomeIcon id={'pickup-btn'} className='text-4xl text-[#436299] transition duration-100 active:scale-90' icon={faGear} />
         </button>
 
-        <div className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-6"></div>
+        <div id={'pickup-sub'} className="sub-line bg-neutral-300 w-[2px] h-[26px] rotate-[12deg] mr-6"></div>
 
         <Link to="/">
-          <button className="home-btn mt-1 mr-9">
-            <FontAwesomeIcon className='text-4xl text-[#436299] transition duration-100 active:scale-90' icon={faHouse} />
+          <button id={'pickup-btn-btn-right'} className="home-btn mt-1 mr-9">
+            <FontAwesomeIcon id={'pickup-btn'} className='text-4xl text-[#436299] transition duration-100 active:scale-90' icon={faHouse} />
           </button>
         </Link>
 
--- src/pages/PickUp.tsx
+++ src/pages/PickUp.tsx
@@ -11,9 +11,11 @@ import Btn_1P from "../assets/1p.png"
 import Point_200 from "../assets/200p.png"
 import PointBack from "../assets/point-back.png"
 import SelectBtn from "../assets/select_student.png"
+import KokonaMini from "./assets2/kokona_minibanner.png"
 
 import connected_sky from "./assets2/connected_sky.wav"
 import Atsuko_PV from "./assets2/atsuko_bg.mp4"
+import Kokona_PV from "./assets2/kokona_pv.mp4"
 import Act10 from "./assets2/act10.mp4"
 
 function PickUp() {
@@ -21,10 +23,6 @@ function PickUp() {
   const [isClick, setIsClick] = useState(false)
   const [is10P, setIs10P] = useState(false)
   const [is1P, setIs1P] = useState(false)
-
-  // const onMusic = () => {
-  //   setIsMusic(true)
-  // }
 
   const onClick = () => {
     setIsClick(true)
@@ -51,7 +49,7 @@ function PickUp() {
       <img className='w-screen h-screen fixed -z-20' src={ Sub_bg } alt="" />
 
       {/* Gacha Act */}
-      { is10P == true || is1P == true ?
+      { is10P || is1P ?
         <video className="h-screen w-screen object-cover z-50 fixed" autoPlay>
           <source src={ Act10 } type="video/mp4" />
         </video>
@@ -62,64 +60,116 @@ function PickUp() {
       </audio>
       
       <video className="h-screen w-screen object-cover -z-10 fixed" muted autoPlay loop>
-        <source src={ Atsuko_PV } type="video/mp4" />
+        <source src={ Kokona_PV } type="video/mp4" />
       </video>
 
       {/* Pick Up Window */}
-      { is10P == false ?
-        <div className="flex justify-end items-center w-screen h-screen text-[#383f46]">
-          <div className="window-bg fixed z-20 w-[680px] h-[580px] bg-[#a9e0f5] rounded-md shadow-md mr-36 mt-[50px]">
-            <div className="pt-3 pb-3 flex justify-center items-center w-[170px] m-[8px] bg-[#f1fbfc] rounded-t-md font-molu text-[23px]">픽업 모집</div>
-            
-            <div className="info w-[680px] h-[445px] -mt-[12px] bg-[#f1fbfc]">
-              <div className="date font-molu text-[23px] pb-6 pt-4 pl-8">2023/01/17 점검 후 ~ 2023/01/31 12:00까지</div>
+      { !is10P ?
+        <div className="flex flex-col justify-center items-end w-screen h-screen text-[#383f46]">
+          <div className="w-[800px]">
+            <img className="mt-[40px] mb-2 -ml-[180px] w-[280px] cursor-pointer transition duration-100 active:scale-95" src={KokonaMini} alt=""/>
+          </div>
 
-              <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8 -mt-6"></div>
-
-              <div className="pickup-text text-[#285986] text-[53px] ml-8 mt-2 mb-1">픽업 모집!</div>
-
-              <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>
-
-              <div className="up-text font-molu text-[23px] ml-8 mt-2 mb-1 text-[#107be3]">아츠코(★3) / 아즈사(★3) 출현 확률 UP!</div>
-            
-              <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>
-
-              <div className="sub-notice pl-8 p-2">
-                <div className="sub-notice-text font-molu text-[21px] text-[#24405d]">10회 모집 시, ★2 이상 학생 1명 확정!</div>
-                <div className="sub-notice-text font-molu text-[21px] text-[#24405d] -mt-1">※ 이미 모집한 학생은 엘리그마와 해당 학생의 엘레프로 변환됩니다.</div>
-              </div>
+          <div className="w-[62px] h-[16px] bg-white rounded-full mr-[500px] mb-[640px] flex justify-around items-center">
+            <div className="ml-1 rounded-full w-[6px] h-[6px] bg-[#6ECBEF]"></div>
+            <div className="rounded-full w-[6px] h-[6px] bg-[#444F55]"></div>
+            <div className="rounded-full w-[6px] h-[6px] bg-[#444F55]"></div>
+            <div className="rounded-full w-[6px] h-[6px] bg-[#444F55]"></div>
+            <div className="mr-1 rounded-full w-[6px] h-[6px] bg-[#444F55]"></div>
+          </div>
+          <div className="window-bg mt-[180px] flex flex-col items-center fixed z-20 w-[900px] h-[600px] bg-[#F1FAFC] rounded-[4px] border-[1.2px] border-gray-300 mr-20">
+            <div className="w-[855px] mt-5 p-1 text-[20px] pl-2 rounded-[2px] bg-[#DAF5FE] text-[#4E646E] font-molu">2023/03/21 11:00부터~2023/03/28 10:59까지</div>
+            <div className="w-[915px] -mt-2">
+              <div className="pickup-text text-[#285986] text-[53px] ml-8 mt-2 ">픽업 모집!</div>
+              <div className="sub-line w-[850px] h-[2px] bg-[#ccd5d9] ml-8"></div>
+              <div className="up-text font-molu text-[23px] ml-9 mt-2 mb-1 text-[#107be3]">★3 코코나 출현 확률 UP!</div>
+              <div className="sub-line w-[850px] h-[2px] bg-[#ccd5d9] ml-8 mb-4"></div>
+              <div className="sub-notice-text font-molu text-[21px] text-[#24405d] ml-9">10회 모집 시, ★2 이상 학생 1명 확정!</div>
+              <div className="sub-notice-text font-molu text-[21px] text-[#24405d] ml-9">※ 이미 모집한 학생은 엘리그마와 해당 학생의 엘레프로 변환됩니다.</div>
 
               {/* Gacha Btn */}
-              <div className="btns flex justify-center items-center mt-12">
+              <div className="btns flex justify-center items-center mt-[99px] mb-6">
 
                 {/* 1회 */}
                 <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick1P }>
-                  <img className="w-[300px] h-[101px]" src={ Btn_1P } alt="" />
+                  <img className="w-[400px] h-[130px]" src={ Btn_1P } alt="" />
                 </button>
 
                 {/* 10회 */}
                 <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick10P }>
-                  <img className="w-[318px] " src={ Btn_10P } alt="" />
+                  <img className="h-[130px]" src={ Btn_10P } alt="" />
                 </button>
                 {/* <Link to="/d10">
                 </Link> */}
               </div>
-  
-            </div>
 
-            <div className="under w-[680px] h-[70px] flex items-center justify-center mt-1">
-              <img className="w-[255px] saturate-150 mr-12" src={ Point_200 } />
-              <div className='point flex justify-center'>
-                <img className='h-[47px] -mt-2 saturate-150 ml-2 absolute' src={ PointBack } />
-                <div className="point-text text-[#fdfdff] font-molu-bold ml-2 z-10 text-3xl">30</div>
+              <div className="under w-[898px] h-[80px] rounded-b-[2px] ml-2 flex items-center justify-center bg-[#A8E0F2]">
+                <img className="w-[255px] saturate-150 mr-12" src={ Point_200 }  alt='' />
+                <div className='point flex justify-center'>
+                  <img className='h-[47px] -mt-2 saturate-150 ml-2 absolute' src={ PointBack } alt='' />
+                  <div className="point-text text-[#fdfdff] font-molu-bold ml-2 z-10 text-3xl">30</div>
+                </div>
+
+                <button className='ml-[80px] transition duration-100 active:scale-90'>
+                  <img className="h-[56px]" src={ SelectBtn } alt="" />
+                </button>
               </div>
 
-              <button className='ml-[70px] transition duration-100 active:scale-90'>
-                  <img className="h-[56px] -mt-[5px]" src={ SelectBtn } alt="" />
-                </button>
             </div>
-
           </div>
+
+          {/*<div id={'pku-window'} className="window-bg fixed z-20 w-[680px] h-[580px] bg-[#a9e0f5] rounded-md shadow-md mr-36 mt-[50px]">*/}
+          {/*  <div id={'pku-tab'} className="pt-3 pb-3 flex justify-center items-center w-[170px] m-[8px] bg-[#f1fbfc] rounded-t-md font-molu text-[23px]">픽업 모집</div>*/}
+          {/*  */}
+          {/*  <div id={'pku-info'} className="info w-[680px] h-[445px] -mt-[12px] bg-[#f1fbfc]">*/}
+          {/*    <div className="date font-molu text-[23px] pb-6 pt-4 pl-8">2023/01/17 점검 후 ~ 2023/01/31 12:00까지</div>*/}
+
+          {/*    <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8 -mt-6"></div>*/}
+
+          {/*    <div className="pickup-text text-[#285986] text-[53px] ml-8 mt-2 mb-1">픽업 모집!</div>*/}
+
+          {/*    <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>*/}
+
+          {/*    <div className="up-text font-molu text-[23px] ml-8 mt-2 mb-1 text-[#107be3]">코코나(★3) 출현 확률 UP!</div>*/}
+          {/*  */}
+          {/*    <div className="sub-line w-[620px] h-[2px] bg-[#ccd5d9] ml-8"></div>*/}
+
+          {/*    <div className="sub-notice pl-8 p-2">*/}
+          {/*      <div className="sub-notice-text font-molu text-[21px] text-[#24405d]">10회 모집 시, ★2 이상 학생 1명 확정!</div>*/}
+          {/*      <div className="sub-notice-text font-molu text-[21px] text-[#24405d] -mt-1">※ 이미 모집한 학생은 엘리그마와 해당 학생의 엘레프로 변환됩니다.</div>*/}
+          {/*    </div>*/}
+
+          {/*    /!* Gacha Btn *!/*/}
+          {/*    <div className="btns flex justify-center items-center mt-12">*/}
+
+          {/*      /!* 1회 *!/*/}
+          {/*      <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick1P }>*/}
+          {/*        <img className="w-[300px] h-[101px]" src={ Btn_1P } alt="" />*/}
+          {/*      </button>*/}
+
+          {/*      /!* 10회 *!/*/}
+          {/*      <button className='m-1 transition duration-100 active:scale-90' onClick={ onClick10P }>*/}
+          {/*        <img className="w-[318px] " src={ Btn_10P } alt="" />*/}
+          {/*      </button>*/}
+          {/*      /!* <Link to="/d10">*/}
+          {/*      </Link> *!/*/}
+          {/*    </div>*/}
+
+          {/*  </div>*/}
+
+          {/*  <div className="under w-[680px] h-[70px] flex items-center justify-center mt-1">*/}
+          {/*    <img className="w-[255px] saturate-150 mr-12" src={ Point_200 } />*/}
+          {/*    <div className='point flex justify-center'>*/}
+          {/*      <img className='h-[47px] -mt-2 saturate-150 ml-2 absolute' src={ PointBack } />*/}
+          {/*      <div className="point-text text-[#fdfdff] font-molu-bold ml-2 z-10 text-3xl">30</div>*/}
+          {/*    </div>*/}
+
+          {/*    <button className='ml-[70px] transition duration-100 active:scale-90'>*/}
+          {/*        <img className="h-[56px] -mt-[5px]" src={ SelectBtn } alt="" />*/}
+          {/*      </button>*/}
+          {/*  </div>*/}
+
+          {/*</div>*/}
         </div>
         : null }
 
